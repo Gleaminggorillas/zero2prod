26e3b99ef6a1765e86040c139bf22dca704079e4
Gleaminggorillas <thomas.henebury@googlemail.com>
2022-04-18 14:26:50 +0100

    update to docker. remember psql and sqlx in init.db

---
+++ scripts/init_db.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+# check for user, else default to 'postgres'
+DB_USER="${POSTGRES_USER:=postgres}"
+# check for password, else default to 'password'
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+# check for custom db, else default to newsletter
+DB_NAME="${POSTGRES_DB:=newsletter}"
+# check if a custom port has been set, else default to 5433
+DB_PORT="${POSTGRES_PORT:=5433}"
+
+# Check psql is installed
+
+#if ! [ -x "$(command -v psql)" ]; then
+#	echo >&2 "Error: psql is not installed."
+#	exit 1
+#fi
+
+# Check sqlx is installed
+
+# if ! [ -x "$(command -v sqlx)" ]; then
+# 	echo >&2 "Error: sqlx is not installed."
+# 	echo >&2 "Use:"
+# 	echo >&2 "	cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
+# 	echo >&2 "to install it"
+# 	exit 1
+# fi
+
+# Launch postgres using docker
+# Allow to skip if a dockerized postgres is running
+if [[ -z "${SKIP DOCKER}" ]]
+then
+docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+
+# ping postgres until its accepting commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do >&2 echo "Postgres is still unavailable - sleeping"
+	sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now."
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
+
--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,25 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // run must be marked as public
 // it is not a binary entrypoint, and can be marked as async
 // without using pro-macro incantation
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
         .listen(listener)?
         .run();
     Ok(server)
