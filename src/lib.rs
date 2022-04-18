55340b27433769772c72e006d5ce727515220fe3
Gleaminggorillas <thomas.henebury@googlemail.com>
2022-04-18 15:14:55 +0100

    refactored files, tests pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,5 @@
 //! lib.rs
 
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
-use std::net::TcpListener;
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-// run must be marked as public
-// it is not a binary entrypoint, and can be marked as async
-// without using pro-macro incantation
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-        .listen(listener)?
-        .run();
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
