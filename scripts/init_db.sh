5b5ca71a3635d69dc200286f5fd672388b98c5a1
tiff <tiffsisstracker@gmail.com>
2022-05-01 14:44:10 +0000

    script adjusted, needs docker/psql ping fix; page 63

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -8,29 +8,29 @@ DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 # check for custom db, else default to newsletter
 DB_NAME="${POSTGRES_DB:=newsletter}"
-# check if a custom port has been set, else default to 5433
-DB_PORT="${POSTGRES_PORT:=5433}"
+# check if a custom port has been set, else default to 5432
+DB_PORT="${POSTGRES_PORT:=5432}"
 
 # Check psql is installed
 
-#if ! [ -x "$(command -v psql)" ]; then
-#	echo >&2 "Error: psql is not installed."
-#	exit 1
-#fi
-
-# Check sqlx is installed
-
-# if ! [ -x "$(command -v sqlx)" ]; then
-# 	echo >&2 "Error: sqlx is not installed."
-# 	echo >&2 "Use:"
-# 	echo >&2 "	cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
-# 	echo >&2 "to install it"
+# if ! [ -x "$(command -v psql)" ]; then
+# 	echo >&2 "Error: psql is not installed."
 # 	exit 1
 # fi
 
+# Check sqlx is installed
+
+if ! [ -x "$(command -v sqlx)" ]; then
+ 	echo >&2 "Error: sqlx is not installed.";
+ 	echo >&2 "Use:";
+ 	echo >&2 "	cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres";
+ 	echo >&2 "to install it";
+	exit 1;
+fi
+
 # Launch postgres using docker
 # Allow to skip if a dockerized postgres is running
-if [[ -z "${SKIP DOCKER}" ]]
+if [[ -z "${SKIP_DOCKER}" ]];
 then
 docker run \
     -e POSTGRES_USER=${DB_USER} \
@@ -38,19 +38,21 @@ docker run \
     -e POSTGRES_DB=${DB_NAME} \
     -p "${DB_PORT}":5432 \
     -d postgres \
-    postgres -N 1000
+    postgres -N 1000;
+fi
 
 # ping postgres until its accepting commands
-export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do >&2 echo "Postgres is still unavailable - sleeping"
-	sleep 1
-done
+# REQUIRES FIX
+# export PGPASSWORD="${DB_PASSWORD}";
+# until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do >&2 echo "Postgres is still unavailable - sleeping";
+# 	sleep 1;
+# done
+# 
+# >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now.";
 
->&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now."
-
-export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME};
 sqlx database create
 sqlx migrate run
 
->&2 echo "Postgres has been migrated, ready to go!"
+>&2 echo "Postgres has been migrated, ready to go!";
 
