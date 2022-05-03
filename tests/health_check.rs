aa9585b19d4e2c248b1a2e6501209cbbed5f1f75
tiff <tiffsisstracker@gmail.com>
2022-05-03 19:58:18 +0000

    page 69, rownotfound

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -45,7 +45,8 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let connection_string = configuration.database.connection_string();
     // Connection trait must be brought into scope to use it
     // PgConnection connect isn't within the struct
-    let connection = PgConnection::connect(&connection_string)
+    // connection must be mutable fo sqlx
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to postgres");
 
@@ -63,6 +64,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
