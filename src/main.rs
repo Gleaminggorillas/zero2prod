6fe9f2ff79d7a6942ae0761f83a400bc5f9a2e4a
tiff <tiffsisstracker@gmail.com>
2022-05-06 20:03:11 +0000

    persist subscriber

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,19 @@
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use sqlx::{Connection, PgConnection};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // get configuration file
     let configuration = get_configuration().expect("Failed to read configuration file");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to postgres");
     // need to use the address variable
     let address = format!("127.0.0.1:{}", configuration.application_port);
     // Bubble up the io::Error if address fails to bind
     // Else, call await
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,15 +2,24 @@ use crate::routes::{health_check, subscribe};
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 // run must be marked as public
 // it is not a binary entrypoint, and can be marked as async
 // without using pro-macro incantation
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(
+    listener: TcpListener,
+    connection: PgConnection
+    ) -> Result<Server, std::io::Error> {
+    // wrap conn in smart pointer
+    let connection = web::Data::new(connection);
+    // grab connection from surrounding env
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // grab the pointer clone, attach to appication state
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
