cbdd0dd4d3fbd09c7577638a4a26fa80e588ef3f
tiff <tiffsisstracker@gmail.com>
2022-05-07 11:44:05 +0000

    dev(app): modify src/startup.rs to use PgPool as opposed to PgConnection

--- src/startup.rs
+++ src/startup.rs
@@ -2,21 +2,21 @@ use crate::routes::{health_check, subscribe};
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgConnection;
+use sqlx::PgPool;
 use std::net::TcpListener;
 // run must be marked as public
 // it is not a binary entrypoint, and can be marked as async
 // without using pro-macro incantation
-pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // wrap conn in smart pointer
-    let connection = web::Data::new(connection);
+    let db_pool = web::Data::new(db_pool);
     // grab connection from surrounding env
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // grab the pointer clone, attach to appication state
-            .app_data(connection.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
