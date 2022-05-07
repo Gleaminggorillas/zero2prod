c067407d1fab86723c32efec4f3b19533a00e087
tiff <thomas.henebury@googlemail.com>
2022-05-07 14:17:08 +0000

    dev(log): add logging functionality

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,9 @@
 use crate::routes::{health_check, subscribe};
 
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 // run must be marked as public
@@ -9,10 +11,12 @@ use std::net::TcpListener;
 // without using pro-macro incantation
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // wrap conn in smart pointer
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     // grab connection from surrounding env
     let server = HttpServer::new(move || {
         App::new()
+            // add middleware logger
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // grab the pointer clone, attach to appication state
