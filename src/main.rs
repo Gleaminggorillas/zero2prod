4b290955d06f0fd678759035a538e9513be71ab7
tiff <thomas.henebury@googlemail.com>
2022-05-08 14:28:33 +0000

    dev(fmt): format recent files

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use zero2prod:: telemetry::{get_subscriber, init_subscriber};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into());
     init_subscriber(subscriber);
-   // get configuration file
+    // get configuration file
     let configuration = get_configuration().expect("Failed to read configuration file");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
