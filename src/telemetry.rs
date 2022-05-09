7adb0c0358cdb6a2fc990601b6b49e962b4708b9
tiff <thomas.henebury@googlemail.com>
2022-05-09 17:31:59 +0000

    dev(uh-oh): no idea what this commit is, but it seems to work

---
+++ src/telemetry.rs
@@ -0,0 +1,32 @@
+//! src/telemetry.rs
+
+use tracing::subscriber::set_global_default;
+use tracing::Subscriber;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+
+/// Bring multiple layers into a tracing subscriber
+///
+/// # Implementation Notes
+///
+/// use 'impl Subscriber' as the return type to avoid
+/// spelling out the actual type of the returned subscriber
+/// which would be complicated.
+/// explicitly call out the returned subscriber uses
+/// Send and Sync, making it possible to pass to init subscriber
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Sync + Send {
+    // remove env_logger
+    // print all spans at info level or above if RUST_LOG hasn't been set
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+pub fn init_subscriber(subscriber: impl Subscriber + Sync + Send) {
+    LogTracer::init().expect("Failed to set logger");
+    set_global_default(subscriber).expect("Failed to set subscriber");
+}
